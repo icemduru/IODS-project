b0cab30855fa9b75fa55d41b32c362d64f882ecc
icemduru <icemduru@gmail.com>
2018-11-09 15:30:17 +0200

    assignment 2 finishing

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -13,3 +13,40 @@ dim(data)
 #checking the data structure
 str(data)
 #looks like there are 60 variables except gender all variables are integer. gender is factor with 2 levels.
+
+#call the library
+library(dplyr)
+
+#Create an analysis dataset with the variables gender, age, attitude, deep, stra, surf and points
+
+#combine questions and create new colums
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+#Creating the new columns and take the average
+deep_columns <- select(data, one_of(deep_questions))
+data$deep <- rowMeans(deep_columns)
+
+surface_columns <- select(data, one_of(surface_questions))
+data$surf <- rowMeans(surface_columns)
+
+strategic_columns <- select(data, one_of(strategic_questions))
+data$stra <- rowMeans(strategic_columns)
+
+#creating attitude column by dividing the values to 10.
+data$attitude <- data$Attitude / 10
+
+#creating learning2014 as results.
+keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
+learning2014 <- select(data, one_of(keep_columns))
+
+#Exclude observations where the exam points variable is zero
+learning2014 <- filter(learning2014, Points > 0)
+
+#check dimentions, structure, and summary
+dim(learning2014)
+str(learning2014)
+summary(learning2014)
+
