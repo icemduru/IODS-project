cdb3337d60e060851e46890a98fe0d98afd6d2be
icemduru <icemduru@gmail.com>
2018-11-09 14:15:46 +0200

    data wrangling assignment

---
+++ data/create_learning2014.R
@@ -0,0 +1,5 @@
+# Name: Ilhan Cem Duru
+# Date: 09.11.2018
+# file description: this is a rscript file for the RStudio Exercise 2, data wrangling section 1.
+
+data <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
