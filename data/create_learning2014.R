e8366ea3f8ed4cbe48b252bc9b8ddd51acda6590
icemduru <icemduru@gmail.com>
2018-11-09 14:24:05 +0200

    finising exercise number 1

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -2,4 +2,14 @@
 # Date: 09.11.2018
 # file description: this is a rscript file for the RStudio Exercise 2, data wrangling section 1.
 
+#pulling the data and read in 
 data <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+
+#checking the data dimentions
+dim(data)
+#looks like there are 183 rows and 60 columns
+
+#checking the data structure
+str(data)
+#looks like there are 60 variables except gender all variables are integer. gender is factor with 2 levels.
